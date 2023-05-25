f33a42bf340591f201023dabc588b085887dc933
koshmelev <koshmelov_da@resanta.ru>
2023-05-25 19:09:42 +0700

    inital server done

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-console.log(123);
+
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const config = require("./serverConfig/config");
+config.config(app);
+
+const PORT = process.env.PORT ?? 4000;
+
+app.listen(PORT, () => {
+  console.log(`Connected on port ${PORT}`);
+});
