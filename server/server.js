7ad5e988f24d1271d41de65b97d7b8f8bb4c7037
Василий Зерщиков <94191911+moonalty@users.noreply.github.com>
2023-05-28 11:41:37 +0300

    DB CONNECTED

--- server/server.js
+++ server/server.js
@@ -6,15 +6,29 @@ const config = require("./serverConfig/config");
 config.config(server);
 //apis
 const usersApi = require('./apis/users/users')
-
+const { Sequelize } = require('sequelize');
 //подключение apis
 
 server.use('/api/cheakyshop/users', usersApi)
 
-
-const PORT = process.env.PORT ?? 4000;
-server.listen(PORT, () => {
-  console.log(`Connected on port ${PORT}`);
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.USER_DATA_DB,  process.env.PASSWORD_DATA_DB, {
+  host: 'localhost',
+  dialect: 'mysql',
 });
+const PORT = process.env.PORT ?? 4000;
 
 
+try{
+  server.listen(PORT, async () => {
+    console.log(`Connected on port ${PORT}`);
+    try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+  });
+} catch(error){
+  console.log(`Something wrong!`);
+  
+}
